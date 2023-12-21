88019d914339cee58bd31721f20f351372e63bcc
Luiz Iury <luiziury_ios@hotmail.com>
2023-12-20 22:35:40 -0300

    Atividade Security

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -28,10 +29,12 @@ public class Postagem {
 	
 	@NotBlank(message = "O atributo título é Obrigatório!") 
 	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	@Column(length = 100)
 	private String titulo;
 	
 	@NotBlank(message = "O atributo texto é Obrigatório!")
 	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 1000 caracteres")
+	@Column(length = 1000)
 	private String texto;
 	
 	@UpdateTimestamp
@@ -40,6 +43,10 @@ public class Postagem {
     @ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+    
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
     public Long getId() {
         return this.id;
@@ -80,5 +87,13 @@ public class Postagem {
     public void setTema(Tema tema) {
         this.tema = tema;
     }
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 }
---
+++ src/main/java/com/generation/blogpessoal/security/JwtService.java
@@ -0,0 +1,70 @@
+package com.generation.blogpessoal.security;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtService {
+
+	public static final String SECRET = "8851f767a9c03adb1fbd9df24cd9fd75f888dd4bc9fa1ba16f502d8aca481766";
+
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parserBuilder()
+				.setSigningKey(getSignKey()).build()
+				.parseClaimsJws(token).getBody();
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+
+	private String createToken(Map<String, Object> claims, String userName) {
+		return Jwts.builder()
+					.setClaims(claims)
+					.setSubject(userName)
+					.setIssuedAt(new Date(System.currentTimeMillis()))
+					.setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+					.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+	}
+
+	public String generateToken(String userName) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userName);
+	}
+
+}
